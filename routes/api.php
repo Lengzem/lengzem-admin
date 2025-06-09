b563764b8ae98aaab29ede248db2438ad88c4604
kailawn-ai <kaialawnaizawl@gmail.com>
2025-06-09 18:32:24 +0530

    Initial commit

--- routes/api.php
+++ routes/api.php
@@ -7,20 +7,18 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\PasswordResetController;
 use App\Http\Controllers\Api\ApiProxyController;
 
-    Route::get('/firebase-user', function () {
+Route::get('/firebase-user', function () {
         return response()->json([
             'user' => auth()->user()
         ]);
     });
 
 Route::get('forgot-password', [PasswordResetController::class, 'create'])->name('password.request');
-
 Route::post('forgot-password', [PasswordResetController::class, 'sendResetLink'])->name('password.reset');
-
 
 Route::post('/proxy', [ApiProxyController::class, 'proxyRequest'])->name('proxy.post');
 Route::get('/proxy', [ApiProxyController::class, 'proxyRequest'])->name('proxy.get');
-Route::get('/proxy', [ApiProxyController::class, 'proxyRequest'])->name('proxy.put');
+Route::put('/proxy', [ApiProxyController::class, 'proxyRequest'])->name('proxy.put');
 
 Route::get('/ProEdit', [RegisterController::class, 'showEdit'])->name('ProEdit');
 Route::get('/login', [LoginController::class, 'showLogin'])->name('login');
